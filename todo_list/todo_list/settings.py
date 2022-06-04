f9600e37e475fe0cd415993453cc57269183cc94
Rajonya Mondal <rajonyarajjo@gmail.com>
2022-06-04 12:15:42 +0530

    Update settings.py

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ul@xavy-b8+prmj03yjg0v!xy58ujlwf%8_x)k&)1u1t&$q!mm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'todolistuserapp.herokuapp.com']
 
 
 # Application definition
